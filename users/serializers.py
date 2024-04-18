ef8e92fb552b6c24f72c3c3d2ee8255c43ec77b3
Mouaness <maroun.mouanes@lau.edu>
2024-04-18 20:11:44 +0300

    small fix on CRUDs for types

--- users/serializers.py
+++ users/serializers.py
@@ -25,22 +25,20 @@ class TypeSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'permissions')
 
     def create(self, validated_data):
-        logger.debug("Received validated data for type creation: %s", validated_data)
-        try:
-            permissions_data = validated_data.pop('permissions', [])
-            with transaction.atomic():
-                type_instance = Type.objects.create(**validated_data)
-                permission_ids = [perm['id'] for perm in permissions_data]
-                permissions = Permission.objects.filter(id__in=permission_ids)
-                if len(permissions) != len(permission_ids):
-                    raise serializers.ValidationError("One or more permissions not found.")
-                type_instance.permissions.set(permissions)
-                return type_instance
-        except Exception as e:
-            logger.error("Error creating type: %s", str(e))
-            raise serializers.ValidationError(str(e))
-    def validate(self, data):
-        # Additional custom validation logic if needed
-        return data
+        permissions_data = validated_data.pop('permissions', [])
+        with transaction.atomic():  # Use atomic transaction
+            type_instance = Type.objects.create(**validated_data)
+            permission_ids = []
+            for perm in permissions_data:
+                try:
+                    permission_ids.append(perm['id'])
+                except KeyError:
+                    raise serializers.ValidationError({'permissions': 'Each permission must include an "id".'})
+            permissions = Permission.objects.filter(id__in=permission_ids)
+            if len(permissions) != len(permission_ids):
+                raise serializers.ValidationError("One or more permissions not found.")
+            type_instance.permissions.set(permissions)  # Link existing permissions by ID
+            return type_instance
+
 
     
