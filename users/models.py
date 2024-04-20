ac240712643eacbfd2ba824bc1824a99b2a55090
Mouaness <maroun.mouanes@lau.edu>
2024-04-21 02:12:51 +0300

    made permission identifier as an enum for CRUD ops

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import enum
 
 class Type(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -12,9 +13,26 @@ class Type(models.Model):
             name='user'
         )
         return type.pk
+
+class CRUDOperations(enum.Enum):
+    CREATE = 'C'
+    READ = 'R'
+    UPDATE = 'U'
+    DELETE = 'D'
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
 class Permission(models.Model):
     name = models.CharField(max_length=255)
-    identifier = models.CharField(max_length=255)
+    identifier = models.CharField(
+        max_length=1,
+        choices=CRUDOperations.choices(),
+        help_text="Type of CRUD operation"
+    )
 
     def __str__(self):
-        return self.name + ' - ' + self.identifier
+        return f"{self.name} - {CRUDOperations(self.identifier).name}"
+
+
