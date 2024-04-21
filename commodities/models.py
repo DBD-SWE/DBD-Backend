7c9bdc60527ba33da98f9a671e0c4148cd5aa14e
Mouaness <maroun.mouanes@lau.edu>
2024-04-21 19:31:18 +0300

    models for attractions

--- commodities/models.py
+++ commodities/models.py
@@ -31,3 +31,11 @@ class GuestHouse(models.Model):
     accessibility = models.BooleanField(default=False)
     food_type = models.CharField(max_length=2, choices=FoodType.choices)
     images = models.ImageField(upload_to='guesthouses_images/', blank=True, null=True)
+
+class Attraction(models.Model):
+    name = models.CharField(max_length=255)
+    location_address = models.CharField(max_length=255)
+    location_coordinates_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    location_coordinates_long = models.DecimalField(max_digits=9, decimal_places=6)
+    description = models.TextField()
+    images = models.ImageField(upload_to='attractions_images/', blank=True, null=True)
