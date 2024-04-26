8d13a33b896bd8734d1e23c85fb3f859d66550b8
Mouaness <maroun.mouanes@lau.edu>
2024-04-26 13:52:28 +0300

    made names unique for districts

--- commodities/models.py
+++ commodities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class District(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
 class Commodity(models.Model):
     name = models.CharField(max_length=255)
