c53b53c1acc920b8b9547697651d07cb8dd056e8
Mouaness <maroun.mouanes@lau.edu>
2024-05-26 22:16:08 +0300

    fix for migration

--- users/signals.py
+++ users/signals.py
@@ -1,8 +1,12 @@
+# users/signals.py
+
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from django.core.management import call_command
 
 @receiver(post_migrate)
 def populate_permissions(sender, **kwargs):
-    # Call the management command to populate permissions
-    call_command('populate_permissions')
+    from django.apps import apps
+    app_config = apps.get_app_config('users')
+    if sender == app_config:
+        call_command('populate_permissions')
